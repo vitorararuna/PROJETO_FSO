f5914c9d26f09c0359270595cd532d30242b326c
vitorararuna <contato.vitorararuna@gmail.com>
2024-08-28 21:27:11 -0300

    connect with backend

---
+++ front/pages/api/apiRoutes.js
@@ -0,0 +1,14 @@
+import api from "./config";
+
+
+// Exemplo de requisição GET:
+
+// export const login = () => {
+//     return api
+//       .get("/login")
+//       .then((response) => response.data)
+//       .catch((error) => {
+//         console.error("Erro ao realziar login:", error);
+//         throw error;
+//       });
+//   };
